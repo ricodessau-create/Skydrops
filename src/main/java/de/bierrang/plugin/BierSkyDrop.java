b08d47c955d873c095b915019dfb92acd40884cc
ricodessau-create <ricodessau@gmail.com>
2026-03-29 10:26:05 +0200

    Create BierSkyDrop.java

---
+++ src/main/java/de/bierrang/plugin/BierSkyDrop.java
@@ -0,0 +1,26 @@
+package de.bierrang.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BierSkyDrop extends JavaPlugin {
+
+    private static BierSkyDrop instance;
+    private DropManager dropManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        dropManager = new DropManager(this);
+        dropManager.load();
+
+        getCommand("skydrop").setExecutor(new SkyCommand(this));
+        getServer().getPluginManager().registerEvents(new DropListener(this), this);
+
+        getLogger().info("BierSkyDrop geladen!");
+    }
+
+    public static BierSkyDrop getInstance() { return instance; }
+    public DropManager getDropManager() { return dropManager; }
+}

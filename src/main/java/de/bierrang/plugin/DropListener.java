579c5019e9380f6b3ff4b751f36e61118488e45c
ricodessau-create <ricodessau@gmail.com>
2026-03-29 10:27:57 +0200

    Create DropListener.java

---
+++ src/main/java/de/bierrang/plugin/DropListener.java
@@ -0,0 +1,39 @@
+package de.bierrang.plugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DropListener implements Listener {
+
+    private final BierSkyDrop plugin;
+
+    public DropListener(BierSkyDrop plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent e) {
+        if (e.getView().getTitle().equals("§cSkyDrop Pool (Items reinlegen)")) {
+            List<ItemStack> newPool = new ArrayList<>();
+            Inventory inv = e.getInventory();
+            
+            for (ItemStack item : inv.getContents()) {
+                if (item != null) {
+                    newPool.add(item.clone());
+                }
+            }
+            
+            plugin.getDropManager().getPool().clear();
+            plugin.getDropManager().getPool().addAll(newPool);
+            plugin.getDropManager().save();
+            
+            e.getPlayer().sendMessage("§aSkyDrop Pool gespeichert! (" + newPool.size() + " Items)");
+        }
+    }
+}
